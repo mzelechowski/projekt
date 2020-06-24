7734f26ae528d13358ebb5067c3909a7e26d32ec
mzelechowski79@gmail.com <Przebisneiga932!@>
2020-06-24 23:04:58 +0200

    K

--- src/com/lotto/Main.java
+++ src/com/lotto/Main.java
@@ -43,34 +43,60 @@ public class Main {
             String inputLine;
             Pattern pLotto = Pattern.compile("<div class=\"scoreline-item circle\" data-v-([a-zA-Z0-9]+)>");
             Pattern pName = Pattern.compile("<p class=\"result-item__name\" data-v-([a-zA-Z0-9]+)>(Lot*[a-zA-Z\\s]+)</p>");
-            //Pattern pData = Pattern.compile("<p class=\"sg__desc-title\" data-v-([a-zA-Z0-9]+)>");
+            Pattern pLosNumber = Pattern.compile("<p class=\"result-item__number\" data-v-([a-zA-Z0-9]+)>\\d{4}</p>");
+            Pattern pEnd=Pattern.compile("</div> </div> <!----> <!----></div> <!----></div>");
             Pattern pData = Pattern.compile("\\d{1,2}\\.\\d{1,2}\\.\\d{4}");
             PrintWriter writer = new PrintWriter(new FileWriter(pathName + fileName4));
             int linia = 0;
+            String outputLine = "";
+            String dataLos="";
             while ((inputLine = in.readLine()) != null) {
-                linia++;
                 Matcher mData = pData.matcher(inputLine);
                 if (mData.find()) {
-                    writer.println(mData.group(0));
+                    dataLos=mData.group(0);
+                    outputLine = dataLos;
                 }
                 /// znajdywanie nazwy
                 Matcher mName = pName.matcher(inputLine);
                 if (mName.find()) {
-                      if (mName.group(0).contains("Lotto Plus")) {
-                        writer.println("Lotto Plus");
-                    } else if (mName.group(0).contains("Lotto"))
-                        writer.println("Lotto");
+                    String pomName = "";
+                    if (mName.group(0).contains("Lotto Plus")) {
+                        //writer.println("Lotto Plus");
+                        pomName = System.getProperty("line.separator")+dataLos +"#Lotto Plus#";
+                    } else if (mName.group(0).contains("Lotto")) {
+                        // writer.println("Lotto");
+                        pomName = "#Lotto#";
+                    }
+                    outputLine = outputLine + pomName;
                 }
-
+                ////Znajdowanie numeru losowania
+                Matcher mNumber = pLosNumber.matcher(inputLine);
+                if (mNumber.find()) {
+                    String pom = mNumber.group(0);
+                    pom = pom.replaceAll("<p class=\"result-item__number\" data-v-[a-zA-Z0-9]+>", "");
+                    pom = pom.replaceAll("</p>", "");
+                    //writer.println(pom);
+                    if(outputLine.endsWith("#"))
+                            outputLine = outputLine + pom+"#";
+                }
+                ////znajdowanie liczb wylosowanych
                 Matcher mLotto = pLotto.matcher(inputLine);
                 if (mLotto.find()) {
                     inputLine = in.readLine();
                     for (int i = 0; i < 50; i++) inputLine = inputLine.replace(" ", "");
-                    writer.println(inputLine);
+                    //writer.println(inputLine);
+                    if(!outputLine.endsWith("#"))
+                        outputLine = outputLine + ",";
+                    outputLine = outputLine  + inputLine;
                 }
-
-
-                writer.println(inputLine);
+                /////////////// send to file
+                Matcher mEnd = pEnd.matcher(inputLine);
+                if (mEnd.find()) {
+                    writer.println(outputLine);
+                    //System.out.println(outputLine);
+                    outputLine="";
+                }
+                //writer.println(inputLine);
             }
             in.close();
             writer.close();
@@ -79,16 +105,3 @@ public class Main {
         }
     }
 }
-
-//scoreline-item circle" data-v-([a-zA-Z0-9]+)>\s+([0-9][0-9]|[0-9])
-//  Pattern p = Pattern.compile("<img [^>]*src=[\"|']([^(\"|')]+)[\"|'][^>]*>");
-//  Matcher m = p.matcher(cnt);
-//while(m.find()) {
-//        System.out.println(m.group(1));
-//        }
-//        String in = "num 123 num 1 num 698 num 19238 num 2134";
-//        Pattern p = Pattern.compile(".*num ([0-9]+)");
-//        Matcher m = p.matcher(in);
-//        if(m.find()) {
-//        System.out.println(m.group(1));
-//        }
