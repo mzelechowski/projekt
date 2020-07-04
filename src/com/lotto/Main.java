8aebe2af5711eff34986a9d2070e3be3ed938394
maciek zelek <mzelechowski79@gmail.com>
2020-07-04 12:40:33 +0200

    z dzis  2020.06.07 gdzo 12:30 druga zmiana

--- src/com/lotto/Main.java
+++ src/com/lotto/Main.java
@@ -14,6 +14,7 @@ import java.util.regex.Pattern;
 
 public class Main {
 // jakas zmiananaksnfks
+    ///druga zmiana
     public static void main(String[] args) {
         String pathName = "C:/!!baza/";
         String fileName = "bazalotto.txt";
